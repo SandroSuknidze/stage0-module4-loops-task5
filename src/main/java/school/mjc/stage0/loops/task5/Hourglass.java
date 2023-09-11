10ef1843669ea2cdb62abbcd2fcda9a5ea5ae1fa
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-11 14:03:01 +0400

    Advanced for loops (changedTwo)

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,41 +2,50 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        for (int i = 0; i < height; i++) {
-            System.out.print("8");
-        }
-        System.out.println();
+        if (height == 0) {
 
-        for (int i = 1; i <= height / 2 - 1; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
+        } else {
+            for (int i = 0; i < height; i++) {
+                System.out.print("8");
             }
-            for (int j = 0; j < height - 2 * i; j++) {
+            System.out.println();
+
+            for (int i = 1; i <= height / 2 - 1; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < height - 2 * i; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+
+            if (height % 2 != 0) {
+                for (int i = 0; i < height / 2; i++) {
+                    System.out.print(" ");
+                }
+                System.out.println("8");
+            } else {
+                for (int i = 0; i < height / 2 - 1; i++) {
+                    System.out.print(" ");
+                }
+                System.out.println("88");
+            }
+
+            for (int i = height / 2 - 1; i >= 1; i--) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < height - 2 * i; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+
+            for (int i = 0; i < height; i++) {
                 System.out.print("8");
             }
             System.out.println();
         }
-
-        if (height % 2 != 0) {
-            for (int i = 0; i < height / 2; i++) {
-                System.out.print(" ");
-            }
-            System.out.println("8");
-        }
-
-        for (int i = height / 2 - 1; i >= 1; i--) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < height - 2 * i; j++) {
-                System.out.print("8");
-            }
-            System.out.println();
-        }
-
-        for (int i = 0; i < height; i++) {
-            System.out.print("8");
-        }
-        System.out.println();
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -3,7 +3,6 @@ package school.mjc.stage0.loops.task5;
 public class Square {
     public void printSquareFrom8s(int sideLength){
         if (sideLength == 0) {
-            System.out.println();
         } else {
             for (int i = 0; i < sideLength; i++) {
                 for (int j = 0; j < sideLength; j++) {
