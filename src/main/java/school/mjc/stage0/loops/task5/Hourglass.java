de639ae35c9f33776c30a46af5e02de4928ddba3
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-11 18:20:04 +0400

    Advanced for loops (changedNine)

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -11,7 +11,7 @@ public class Hourglass {
 
             for (int i = 1; i <= height / 2 - 1; i++) {
                 for (int j = 0; j < i; j++) {
-                    System.out.print(" ");
+                    System.out.print("\u0020");
                 }
                 for (int j = 0; j < height - 2 * i; j++) {
                     System.out.print("8");
@@ -21,14 +21,14 @@ public class Hourglass {
 
             if (height % 2 != 0) {
                 for (int i = 0; i < height / 2; i++) {
-                    System.out.print(" ");
+                    System.out.print("\u0020");
                 }
                 System.out.println("8");
             }
 
             for (int i = height / 2 - 1; i >= 1; i--) {
                 for (int j = 0; j < i; j++) {
-                    System.out.print(" ");
+                    System.out.print("\u0020");
                 }
                 for (int j = 0; j < height - 2 * i; j++) {
                     System.out.print("8");
